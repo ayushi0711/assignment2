0cd77f13462d69f420955cb5800dc1490f526f0e
ayushi <ayushiyadav7575@gmail.com>
2023-11-16 23:00:15 +0530

    first commit

---
+++ Q5.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+ char ch1,ch2;
+ cout<<"Enter First Character";
+ cin>>ch1;
+ cout<<"Enter First Character";
+ cin>>ch2;
+ cout<<"the difference between two character ASCII is"<<ch1-ch2; 
+
+    return 0;
+} 
